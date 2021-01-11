209af4d74f5690a82395ce9897a9cb93c6be592b
noel <68936@naver.com>
2021-01-11 12:18:42 +0900

    Single Linked List 구현 아직 헤더를 지울 순 없음.

---
+++ src/LinkedList/Single/Single.java
@@ -0,0 +1,58 @@
+package LinkedList.Single;
+
+public class Single{
+
+    private int data = 0;
+    private Single next = null;
+
+    Single(int d){
+        this.data = d;
+    }
+
+    void append(int d){
+        Single node_end = new Single(d);
+        Single node = this;
+
+        while (node.next != null){
+            node = node.next;
+        }
+        node.next = node_end;
+    }
+
+    void delete(int d){
+        Single node = this;
+
+        while(node.next != null){
+            if(node.next.data == d){
+                node.next = node.next.next;
+            }
+            else{
+                node = node.next;
+            }
+
+        }
+    }
+
+    void retrieve(){
+        Single node = this;
+
+        while (node.next != null){
+
+            System.out.print(node.data+" -> ");
+            node = node.next;
+        }
+        System.out.println(node.data);
+
+    }
+
+
+    public static void main(String[] args) {
+        Single head = new Single(1);
+        head.append(2);
+        head.append(3);
+        head.append(4);
+        head.retrieve();
+        head.delete(3);
+        head.retrieve();
+    }
+}
